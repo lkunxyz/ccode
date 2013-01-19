b07095e4a51233ddbdb640db7dc2a6098aad6f82
lkunxyz <lkunemail@163.com>
2013-01-19 12:48:14 +0800

    commit message

---
+++ cmdd.sh
@@ -0,0 +1,23 @@
+time = `date +%Y/%m/%d-%X`
+
+
+
+echo '-----------   git add .   -------------------'
+git add .
+
+echo
+echo
+echo '--- git commit -m message for commit $time-------'
+git commit -m 'commit message'
+
+
+echo
+echo
+echo '---  git push -u origin master --------------'
+git push -u origin master
+
+
+echo
+echo
+echo '------ All operations excute end.------------'cho $time
+
